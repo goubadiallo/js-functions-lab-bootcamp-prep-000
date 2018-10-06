5f02fb89bcfa92e6d8cf220a2ba9a2e7f9015be9
Mamadou Diallo <mamadou.diallo@wework.com>
2018-10-06 00:01:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+function happyHolidays(name){
+  return 'Happy holidays!'
+
+}
+happyHolidays('Happy holidays!')
+  
+
+function happyHolidaysTo(name){
+happyHolidaysTo("Happy holidays, you!")
+}
+happyHolidaysTo()
+
+
+ 
+ function holidayCountdown(holiday, days){
+   return "It's 20 days until Mother's Day!"
+   
+ }
+ holidayCountdown("Mother's Day")
+
