81071305b1ca974537c192f0d19ff0dcd6684adb
Mamadou Diallo <mamadou.diallo@wework.com>
2018-10-06 00:08:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function happyHolidaysTo(name){
 return "Happy holidays, you!"
 }
 happyHolidaysTo("Happy holidays, you!")
-var str
+var str  = '${holiday}, ${name}'
 
  
  function holidayCountdown(holiday, days){
